e9a4789a15f12e767293134dbb40f3c2ba813f02
Antoine Meyer <antoine.meyer@u-pem.fr>
2022-09-11 19:35:02 +0200

    Passe sur chapitre 1

--- 1-valeurs-variables-types/code/pancakes.py
+++ 1-valeurs-variables-types/code/pancakes.py
@@ -26,31 +26,6 @@ u_lait = 'cl'
 q_oeuf = 1
 u_oeuf = 'unité(s)'
 
-texte_recette = '''
-Étape 1 (beurre) :
-Faire fondre le beurre, dans une casserole à feu doux ou dans un bol au
-micro-ondes.
-
-Étape 2 (farine, levure, sucre) :
-Mettre la farine, la levure et le sucre dans un saladier. Mélanger et
-creuser un puits.
-
-Étape 3 (oeuf) :
-Ajouter ensuite les oeufs entiers et fouetter l'ensemble.
-
-Étape 4 (beurre, lait) :
-Incorporer le beurre fondu, fouetter puis délayer progressivement le
-mélange avec le lait afin d'éviter les grumeaux.
-
-Étape 5 :
-Laisser reposer la pâte au minimum 1 heure au réfrigérateur.
-
-Étape 6 :
-Dans une poêle chaude et légèrement huilée, faire cuire comme des crêpes,
-mais en les faisant plus petites. Réserver au chaud et déguster.
-'''
-
-
 if __name__ == '__main__':
     nb_convives = int(input('Combien de personnes ? '))
     print('Pour faire', nom_recette, 
@@ -63,5 +38,3 @@ if __name__ == '__main__':
     print(q_sel * rapport, u_sel, 'de sel')
     print(q_lait * rapport, u_lait, 'de lait')
     print(q_oeuf * rapport, u_oeuf, 'de oeuf')
-    
-    print(texte_recette)
